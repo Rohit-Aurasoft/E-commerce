dc21569d92f52bdd128ac5fa5a131416c5fba4dd
Rohit-Aurasoft <rohit.aurasoft@gmail.com>
2022-12-14 04:52:43 +0530

    counter,removeitem

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+///////////////////////// ALL Products //////////////////////////
 const products = [
     {
         dataid: 1,
@@ -33,7 +34,7 @@ const productlist = () => {
                      <h2 class="product-title">${item.Pro_name}</h2>
                      <p class="description">${item.discription}</p>
                      <span class="price">$${item.price}</span>
-                     <button type="button" class="btn-cart add-cart">Add To Cart</button>
+                     <button type="button" class="btn-cart add-cart" >Add To Cart</button>
                  </div>
                         `;
     })
@@ -43,12 +44,23 @@ const productlist = () => {
 productlist();
 
 ///////////////////////////// C A R T ///////////////////////////
+
 let cartIcon = document.querySelector("#cart-icon")
 let cart = document.querySelector(".cart")
 let closeCart = document.querySelector("#close-cart")
+
 ////////////////////////////////// O P E N-C A R T ////////////////////////
 cartIcon.onclick = () => {
     cart.classList.add("active")
+    additemtoUI();
+    const removecartbtn = document.querySelectorAll(".cart-remove")
+    console.log("removecartbtn", removecartbtn)
+    removecartbtn.forEach((btn, i) => {
+        console.log("btn", btn)
+        btn.addEventListener('click', () => {
+            remove(i)
+        })
+    })
 }
 
 //////////////////////////////////// C L O S E-C A R T /////////////////////////////
@@ -65,52 +77,67 @@ addcartbtn.forEach((btn, i) => {
         addcart(i)
     })
 })
+
+
 function addcart(i) {
 
-    ///////to insert item by there id///////////
     let itemToInsert = products.find(item => item.dataid === i + 1);
-    console.log("itemToInsert", itemToInsert);
-    ///////////////it push the item in empty array///////////////////
     let a = [];
-    // Parse the serialized data back into an aray of objects
     a = JSON.parse(localStorage.getItem('cartItems')) || [];
-    console.log("a", a)
-    //
-    let duplicate = a.find(item => item.dataid === i+1);
-    console.log("duplicate", duplicate);
+    let duplicate = a.find(item => item.dataid === i + 1);
+
     if (duplicate === undefined) {
         a.push(itemToInsert);
+        localStorage.setItem('cartItems', JSON.stringify(a));
+        counter()
+    } else {
+        alert("your Item aready exist, If you want to increase than go to cart");  // Should be something like [Object array]
     }
-    // Push the new data (whether it be an object or anything else) onto the array
-    // a.push(itemToInsert);
-    // Alert the array value
-    alert("your Item aready exist, If you want to increase than go to cart");  // Should be something like [Object array]
-    // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('cartItems', JSON.stringify(a));
-    
 
-
-    additemincart();
 }
+counter()
 
-
-function additemincart() {
-    let cartitem = JSON.parse(localStorage.getItem('cartItems'));
-    console.log('cartitem', cartitem)
+function additemtoUI() {
     let itemdata = document.querySelector(".cart-content")
-    console.log("product", itemdata)
+    itemdata.innerHTML = ""
+
+    let cartitem = JSON.parse(localStorage.getItem('cartItems'));
+
     cartitem.map(item => {
         itemdata.innerHTML += `
                 <div class="cart-box" dataid="${item.dataid}">
                     <img src=${item.image} alt="" class="cart-img"/>
                      <h2 class="cart-product-title">${item.Pro_name}</h2>
-                     <i class='bx bxs-message-alt-x cart-remove'></i>
+                     <i class='bx bxs-message-alt-x cart-remove' ></i>
                      <span class="cart-price">$${item.price}</span>
-                     <div id="cart-quantity"><button class="value-button increase-btn" id="decrease" onclick="decreaseValue()" value="Increase Value">-</button><input type="number" id="number" value="0" /><button class="value-button decrease-btn" id="increase" onclick="increaseValue()" value="Increase Value">+</button></div>
-                 </div>
-                
-                        `;
+                     <div id="cart-quantity">
+                     <button class="value-button increase-btn" id="decrease" onclick="decreaseValue()" value="Increase Value">-</button>
+                     <input type="number" id="number" value="0" />
+                     <button class="value-button decrease-btn" id="increase" onclick="increaseValue()" value="Increase Value">+</button>
+                     </div>
+                 </div>`;
     })
-
 }
-additemincart();
+
+//////////removeitem//////////////////////////
+function remove(i) {
+    const items = JSON.parse(localStorage.getItem('cartItems'));
+
+    items.splice(i, 1)
+
+
+    localStorage.setItem("cartItems", JSON.stringify(items))
+    location.reload();
+    additemtoUI();
+}
+
+function counter() {
+    let n = JSON.parse(localStorage.getItem('cartItems'))
+
+    document.getElementById('counter').innerHTML = n.length;
+}
+
+
+
+
+// @uraSoft@103
